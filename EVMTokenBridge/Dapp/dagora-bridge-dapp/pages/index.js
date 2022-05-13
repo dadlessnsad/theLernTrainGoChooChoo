f2b3ee84ed7d529e53a50f87fc5907c57e998e9b
dadlessnsad <tbodnar85@gmail.com>
2022-05-12 22:12:27 -0600

    bridge dapp framework

---
+++ EVMTokenBridge/Dapp/dagora-bridge-dapp/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>dAgora Bridge</title>
+        <meta name="description" content="hopefully a evm bridge idk" />
+        <link rel="icon" href="/favicon.ico" />"
+      </Head>
+
+      <main className="flex flex-col justify-center items-center min-h-screen bg-vet-pepe bg-contain">
+        <h1 className="z-1 text-7xl font-bold text-blue-500 pb-5">
+          Welcome to the dAgora Bridge
+        </h1>
+        <p className="z-1 text-base pt-5">
+        ......SOOOOOOOOOOOON......
+        </p>
+      </main>
+
+
+    </div>
+  )
+}
---
+++ EVMTokenBridge/Dapp/dagora-bridge-dapp/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'vet-pepe': "url('../public/vet.jpeg')"
+      },
+    },
+  },
+  plugins: [],
+}
